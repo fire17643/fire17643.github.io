80d234c9bf4089d6c3a955406b76da332081f700
fire17643 <1258017643@qq.com>
2018-11-18 23:03:50 +0800

    add notes

---
+++ notes/bag2.js
@@ -0,0 +1,73 @@
+var sizeof = function(str, charset){
+    var total = 0,
+        charCode,
+        i,
+        len;
+    charset = charset ? charset.toLowerCase() : '';
+    if(charset === 'utf-16' || charset === 'utf16'){
+        for(i = 0, len = str.length; i < len; i++){
+            charCode = str.charCodeAt(i);
+            if(charCode <= 0xffff){
+                total += 2;
+            }else{
+                total += 4;
+            }
+        }
+    }else{
+        for(i = 0, len = str.length; i < len; i++){
+            charCode = str.charCodeAt(i);
+            if(charCode <= 0x007f) {
+                total += 1;
+            }else if(charCode <= 0x07ff){
+                total += 2;
+            }else if(charCode <= 0xffff){
+                total += 3;
+            }else{
+                total += 4;
+            }
+        }
+    }
+    return total;
+}
+
+function isEmptyObj(obj)
+{
+    for(var i in obj){
+        if(obj.hasOwnProperty(name)){
+            return false;
+        }
+    }
+    return true;
+};
+
+// 数组去重
+function unique(a) {
+  return Array.from(new Set(a));
+}
+ 
+var a = [{name: "hanzichi"}, 1, "1", {age: 30}, new String(1), new Number(1)];
+var ans = unique(a);
+console.log(ans); // => [Object, Object, String, Number]
+
+// 防抖动函数
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate & !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+ 
+var myEfficientFn = debounce(function() {
+    // 滚动中的真正的操作
+}, 250);
+ 
+// 绑定监听
+window.addEventListener('resize', myEfficientFn);
